e982063d5dca9f59bcb5545b6cc2cab4395d70a7
alebri0616 <alebri0616@skola.goteborg.se>
2025-12-09 23:12:17 +0100

    added an input field and a button where you can search for a character and their image will pop up below

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
+
+const button = document.querySelector("button");
+const image = document.querySelector("img");
 async function fetchData (){
+
     try{
-        const response = await fetch("https://rickandmortyapi.com/api/character/?name=rick&status=alive")
+
+        const character  = document.getElementById("character").value.toLowerCase();
+        
+        
+        const response = await fetch(`https://rickandmortyapi.com/api/character/?name=${character}&status=alive`)
         if (!response.ok) {
             throw new Error ("Could'nt fetch the resource.");
         }
         const data = await response.json();
-        console.log(data.results);
+        console.log(data.results[0]);
+        image.src = data.results[0].image;
     }
     catch(error){
         console.error(error);
     }
 } 
 
-fetchData();
+button.addEventListener('click', () => {
+    fetchData();
+    image.style = "block";
+});

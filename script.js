62a8e56e43a37ded2dda61802cec09a0c9d3b64d
alebri0616 <alebri0616@skola.goteborg.se>
2025-12-10 00:29:11 +0100

    added information about the specific character under the image like: status, species and full name

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 
 const button = document.querySelector("button");
 const image = document.querySelector("img");
+const gender = document.getElementById("gender");
+const fullname = document.getElementById("fullname");
+const species = document.getElementById("species");
+const characterStatus = document.getElementById("status");
+
 async function fetchData (){
 
     try{
@@ -8,13 +13,19 @@ async function fetchData (){
         const character  = document.getElementById("character").value.toLowerCase();
         
         
-        const response = await fetch(`https://rickandmortyapi.com/api/character/?name=${character}&status=alive`)
+        const response = await fetch(`https://rickandmortyapi.com/api/character/?name=${character}`)
         if (!response.ok) {
             throw new Error ("Could'nt fetch the resource.");
         }
         const data = await response.json();
-        console.log(data.results[0]);
+        
         image.src = data.results[0].image;
+        image.style = "block";
+        console.log(data.results)
+        gender.textContent = "Gender: " + data.results[0].gender;
+        fullname.textContent = "Full Name: " + data.results[0].name;
+        species.textContent = "Species: " + data.results[0].species;
+        characterStatus.textContent = "Status: " + data.results[0].status;
     }
     catch(error){
         console.error(error);
@@ -23,5 +34,6 @@ async function fetchData (){
 
 button.addEventListener('click', () => {
     fetchData();
-    image.style = "block";
+    
+    
 });
